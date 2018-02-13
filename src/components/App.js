4aeda0b88b2cd5b6bfab5156b16783f7c8e0ca0f
Cameron Jenkinson <camjenkinson@gmail.com>
2018-02-13 09:47:17 +0100

    Update file names, containers to components, added styles folder, __tests___

---
+++ src/components/App.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import './styles/main.css';
+
+const App = () => <div><h1>React Boilerplate</h1></div>
+
+export default App
--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 /* eslint-env browser */
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './containers/App';
+import App from './components/App';
 
 ReactDOM.render(
   <App />,
